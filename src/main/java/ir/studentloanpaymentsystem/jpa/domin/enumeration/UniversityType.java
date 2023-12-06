c62c1585b7c4ccd6dfb2b5257621eb01b5b24487
HoseinRezaei <rezaeen77@gmail.com>
2023-12-06 16:46:18 +0330

    Create UniversityType

---
+++ src/main/java/ir/studentloanpaymentsystem/jpa/domin/enumeration/UniversityType.java
@@ -0,0 +1,10 @@
+package ir.studentloanpaymentsystem.jpa.domin.enumeration;
+
+public enum UniversityType {
+    AzadUniversity,
+    NonprofitUniversity,
+    Pardis,
+    ZarfiatMazad,
+    PayamNoor,
+    ElmiKaarbordi
+}
