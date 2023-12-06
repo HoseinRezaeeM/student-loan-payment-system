e0103438521c679bcabd21a657c4cc8a55554580
HoseinRezaei <rezaeen77@gmail.com>
2023-12-06 16:45:27 +0330

    Create Degree Enum

---
+++ src/main/java/ir/studentloanpaymentsystem/jpa/domin/enumeration/Degree.java
@@ -0,0 +1,11 @@
+package ir.studentloanpaymentsystem.jpa.domin.enumeration;
+
+public enum Degree {
+    Associate,
+    Bachelor,
+    NoncontiguousBachelor,
+    Master,
+    Doctor,
+    ProfessionalDoctor,
+    NonContinuousSpecializedDoctor,
+}
