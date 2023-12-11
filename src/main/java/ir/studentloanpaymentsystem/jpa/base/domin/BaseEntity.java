77c117b3a5928fd958594305b1feb68db98b952c
HoseinRezaei <rezaeen77@gmail.com>
2023-12-11 16:28:23 +0330

    Create BaseEntity

--- src/main/java/ir/studentloanpaymentsystem/jpa/base/domin/BaseEntity.java
+++ src/main/java/ir/studentloanpaymentsystem/jpa/base/domin/BaseEntity.java
@@ -1,17 +1,15 @@
 package ir.studentloanpaymentsystem.jpa.base.domin;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
 import java.io.Serializable;
 @MappedSuperclass
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class BaseEntity<ID extends Serializable> {
