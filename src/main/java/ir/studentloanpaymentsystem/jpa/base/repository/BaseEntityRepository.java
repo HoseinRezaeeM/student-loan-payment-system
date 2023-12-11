dc2dbcbf55c4db3890bd27bc3c0085c432afc4da
HoseinRezaei <rezaeen77@gmail.com>
2023-12-11 16:28:46 +0330

    Create BaseEntityRepository for CRUD

---
+++ src/main/java/ir/studentloanpaymentsystem/jpa/base/repository/BaseEntityRepository.java
@@ -0,0 +1,24 @@
+package ir.studentloanpaymentsystem.jpa.base.repository;
+
+import ir.studentloanpaymentsystem.jpa.base.domin.BaseEntity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Optional;
+
+public interface BaseEntityRepository<ID extends Serializable,T extends BaseEntity<ID>> {
+
+    T save(T entity);
+
+    void deleteById(ID id);
+
+    Collection<T> findAll();
+
+    Optional<T> findById(ID id);
+
+    void beginTransaction();
+
+    void commitTransaction();
+
+    void rollBack();
+}
