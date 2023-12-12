5a4eb77f3a22917046b95edc421e8260efb03f94
HoseinRezaei <rezaeen77@gmail.com>
2023-12-12 17:00:51 +0330

    Edit BaseEntityRepositoryImpl

---
+++ src/main/java/ir/studentloanpaymentsystem/jpa/base/repository/impl/BaseEntityRepositoryImpl.java
@@ -0,0 +1,81 @@
+package ir.studentloanpaymentsystem.jpa.base.repository.impl;
+
+
+import ir.studentloanpaymentsystem.jpa.base.domin.BaseEntity;
+import ir.studentloanpaymentsystem.jpa.base.repository.BaseEntityRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Optional;
+
+public abstract class BaseEntityRepositoryImpl<ID extends Serializable, T extends BaseEntity<ID>> implements BaseEntityRepository<ID, T> {
+
+    protected EntityManager entityManager;
+
+    public BaseEntityRepositoryImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public T save(T entity) {
+        beginTransaction();
+        entity = saveWithoutTransaction(entity);
+        commitTransaction();
+        return entity;
+    }
+
+    private T saveWithoutTransaction(T entity) {
+        if (entity.getId() == null)
+            entityManager.persist(entity);
+        else
+            entity = entityManager.merge(entity);
+        return entity;
+    }
+
+    @Override
+    public void deleteById(ID id) {  // By Optional
+        beginTransaction();
+        Optional<T> optional = findById(id);
+        optional.ifPresent(entityManager::remove);
+        commitTransaction();
+    }
+
+    @Override
+    public Collection<T> findAll() {  // By Criteria
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<T> criteriaQuery =criteriaBuilder.createQuery(getEntityClass());
+        Root<T> from = criteriaQuery.from(getEntityClass());
+        CriteriaQuery<T> select =criteriaQuery.select(from);
+        return entityManager.createQuery(select).getResultList();
+    }
+
+    @Override
+    public Optional<T> findById(ID id) {
+        return Optional.ofNullable(entityManager.find(getEntityClass(),id));
+    }
+
+    public abstract Class<T> getEntityClass();
+
+    @Override
+    public void beginTransaction() {
+        if (!entityManager.getTransaction().isActive())
+            entityManager.getTransaction().begin();
+    }
+
+    @Override
+    public void commitTransaction() {
+        if (entityManager.getTransaction().isActive())
+            entityManager.getTransaction().commit();
+
+    }
+
+    @Override
+    public void rollBack() {
+        if (entityManager.getTransaction().isActive())
+            entityManager.getTransaction().rollback();
+    }
+}
