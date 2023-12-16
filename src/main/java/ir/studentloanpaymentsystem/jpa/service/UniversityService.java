5320182b4e60ee70b896c856984b80e4de126f6f
HoseinRezaei <rezaeen77@gmail.com>
2023-12-16 18:10:11 +0330

    Create UniversityService

---
+++ src/main/java/ir/studentloanpaymentsystem/jpa/service/UniversityService.java
@@ -0,0 +1,7 @@
+package ir.studentloanpaymentsystem.jpa.service;
+
+import ir.studentloanpaymentsystem.jpa.base.service.BaseEntityService;
+import ir.studentloanpaymentsystem.jpa.domin.University;
+
+public interface UniversityService extends BaseEntityService<Integer, University> {
+}
