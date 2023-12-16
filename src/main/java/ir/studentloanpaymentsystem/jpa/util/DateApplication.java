798c14234ce0868e79bd7be22e16e99d18aff22a
HoseinRezaei <rezaeen77@gmail.com>
2023-12-16 18:07:58 +0330

    Create DataApplication for Date Conste

--- src/main/java/ir/studentloanpaymentsystem/jpa/util/DateApplication.java
+++ src/main/java/ir/studentloanpaymentsystem/jpa/util/DateApplication.java
@@ -3,15 +3,15 @@ package ir.studentloanpaymentsystem.jpa.util;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static ir.studentloanpaymentsystem.jpa.menu.BaseMenu.scanner;
+
 
 
 public class DateApplication {
-    public static final LocalDate DATEAPLICATION =LocalDate.of(1401, 7,8);
+    public static final LocalDate DATENOWAPLICATION =LocalDate.of(1402, 12,27);
 
     public static LocalDate addDate(String date) {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-        return LocalDate.parse(date, dtf);
+        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        return LocalDate.parse(date, pattern);
     }
 
 }
