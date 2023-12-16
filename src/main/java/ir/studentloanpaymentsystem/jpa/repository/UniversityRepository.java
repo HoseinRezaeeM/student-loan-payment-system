3cae9c874be9f73a678d28b2a5aeefea637401e1
HoseinRezaei <rezaeen77@gmail.com>
2023-12-16 18:09:53 +0330

    Create UniversityRepository

---
+++ src/main/java/ir/studentloanpaymentsystem/jpa/repository/UniversityRepository.java
@@ -0,0 +1,7 @@
+package ir.studentloanpaymentsystem.jpa.repository;
+
+import ir.studentloanpaymentsystem.jpa.base.repository.BaseEntityRepository;
+import ir.studentloanpaymentsystem.jpa.domin.University;
+
+public interface UniversityRepository extends BaseEntityRepository<Integer, University> {
+}
