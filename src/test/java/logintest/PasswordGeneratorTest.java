392f974c48ddb55d9ba19c7c8ba4434bbc22018d
HoseinRezaei <rezaeen77@gmail.com>
2023-12-16 18:04:09 +0330

    Create PasswordGeneratorTest for test

---
+++ src/test/java/logintest/PasswordGeneratorTest.java
@@ -0,0 +1,14 @@
+package logintest;
+
+import ir.studentloanpaymentsystem.jpa.util.PasswordGenerator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PasswordGeneratorTest {
+
+    @Test
+    void generatePassword() {
+        System.out.println(PasswordGenerator.generatePassword());
+        Assertions.assertNotNull(PasswordGenerator.generatePassword());
+    }
+}
