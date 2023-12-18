3d1be1c5b90786198ef1c6a434085f824b03754b
HoseinRezaei <rezaeen77@gmail.com>
2023-12-18 11:59:54 +0330

    create MarriedRepository

---
+++ src/main/java/ir/studentloanpaymentsystem/jpa/repository/MarriedRepository.java
@@ -0,0 +1,7 @@
+package ir.studentloanpaymentsystem.jpa.repository;
+
+import ir.studentloanpaymentsystem.jpa.base.repository.BaseEntityRepository;
+import ir.studentloanpaymentsystem.jpa.domin.Married;
+
+public interface MarriedRepository extends BaseEntityRepository<Integer,Married> {
+}
