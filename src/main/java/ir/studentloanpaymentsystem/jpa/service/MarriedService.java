cf6981a10d082f764c3dcb14807f70b4435bb83f
HoseinRezaei <rezaeen77@gmail.com>
2023-12-18 12:00:11 +0330

    create MarriedService

---
+++ src/main/java/ir/studentloanpaymentsystem/jpa/service/MarriedService.java
@@ -0,0 +1,7 @@
+package ir.studentloanpaymentsystem.jpa.service;
+
+import ir.studentloanpaymentsystem.jpa.base.service.BaseEntityService;
+import ir.studentloanpaymentsystem.jpa.domin.Married;
+
+public interface MarriedService extends BaseEntityService<Integer, Married> {
+}
