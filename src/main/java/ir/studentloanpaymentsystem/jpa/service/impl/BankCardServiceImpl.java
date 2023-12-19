8cc4750b5f72e91b78f7bca2670befbffba6b169
HoseinRezaei <rezaeen77@gmail.com>
2023-12-19 12:27:30 +0330

    create BankCardServiceImpl

---
+++ src/main/java/ir/studentloanpaymentsystem/jpa/service/impl/BankCardServiceImpl.java
@@ -0,0 +1,28 @@
+package ir.studentloanpaymentsystem.jpa.service.impl;
+
+import ir.studentloanpaymentsystem.jpa.base.service.impl.BaseEntityServiceImpl;
+import ir.studentloanpaymentsystem.jpa.domin.BankCard;
+import ir.studentloanpaymentsystem.jpa.domin.Student;
+import ir.studentloanpaymentsystem.jpa.repository.BankCardRepository;
+import ir.studentloanpaymentsystem.jpa.service.BankCardService;
+
+public class BankCardServiceImpl extends BaseEntityServiceImpl<Integer, BankCard, BankCardRepository> implements BankCardService {
+    public BankCardServiceImpl(BankCardRepository baseRepository) {
+        super(baseRepository);
+    }
+
+    @Override
+    public boolean existsByNumberCard(String numberCard) {
+        return baseRepository.existsByNumberCard(numberCard);
+    }
+
+    @Override
+    public boolean existBanCardByNumberCardAndCcvAndExpirationDate(String numberCard, String cvv, String expirationDate) {
+        return baseRepository.existBanCardByNumberCardAndCcvAndExpirationDate(numberCard,cvv,expirationDate);
+    }
+
+    @Override
+    public boolean hasBankCardForStudent(Student student) {
+        return baseRepository.hasBankCardForStudent(student);
+    }
+}
